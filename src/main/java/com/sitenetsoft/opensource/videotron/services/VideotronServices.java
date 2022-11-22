0e60480da2e3ac9625b2e1a6d25884d9bc7affa5
Jean-Nicolas Boulay <jn@yaloub.com>
2022-11-22 01:52:51 -0500

    Added event and service code

---
+++ src/main/java/com/sitenetsoft/opensource/videotron/services/VideotronServices.java
@@ -0,0 +1,61 @@
+package com.sitenetsoft.opensource.videotron.services;
+
+import java.util.Map;
+
+import org.apache.ofbiz.base.util.Debug;
+import org.apache.ofbiz.entity.Delegator;
+import org.apache.ofbiz.entity.GenericEntityException;
+import org.apache.ofbiz.entity.GenericValue;
+import org.apache.ofbiz.service.DispatchContext;
+import org.apache.ofbiz.service.ServiceUtil;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class VideotronServices {
+
+    public static final String module = VideotronServices.class.getName();
+
+    public static Map<String, Object> createOfbizDemo(DispatchContext dctx, Map<String, ? extends Object> context) {
+        Map<String, Object> result = ServiceUtil.returnSuccess();
+        Delegator delegator = dctx.getDelegator();
+        try {
+
+            try {
+                Document doc = Jsoup.connect("http://example.com").get();
+                doc.select("p").forEach(System.out::println);
+                System.out.println(doc.select("p").toString());
+            } catch (Exception exception) {
+                System.out.println(exception);
+            }
+
+            GenericValue ofbizDemo = delegator.makeValue("Videotron");
+            // Auto generating next sequence of ofbizDemoId primary key
+            ofbizDemo.setNextSeqId();
+            // Setting up all non primary key field values from context map
+            ofbizDemo.setNonPKFields(context);
+            // Creating record in database for OfbizDemo entity for prepared value
+            ofbizDemo = delegator.create(ofbizDemo);
+            result.put("videotronId", ofbizDemo.getString("videotronId"));
+            Debug.log("==========This is my first Java Service implementation in Apache OFBiz. Videotron record created successfully with ofbizDemoId: "+ofbizDemo.getString("ofbizDemoId"));
+        } catch (GenericEntityException e) {
+            Debug.logError(e, module);
+            return ServiceUtil.returnError("Error in creating record in OfbizDemo entity ........" +module);
+        }
+        return result;
+
+        /*Connection.Response loginForm = Jsoup.connect("https://www.desco.org.bd/ebill/login.php")
+        .method(Connection.Method.GET)
+        .execute();
+
+        Document document = Jsoup.connect("https://www.desco.org.bd/ebill/authentication.php")
+        .data("cookieexists", "false")
+        .data("username", "32007702")
+        .data("login", "Login")
+        .cookies(loginForm.cookies())
+        .post();
+        System.out.println(document);*/
+    }
+
+}
