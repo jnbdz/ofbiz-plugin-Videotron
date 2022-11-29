53065626b26bcb7a9e29468d510d38fb6903443b
Jean-Nicolas Boulay <jn@yaloub.com>
2022-11-29 00:36:44 -0500

    Added some of the URLs for the client center and added login form inputs. Also added the jsoupe code (not working) for login.

--- src/main/java/com/sitenetsoft/opensource/videotron/services/VideotronServices.java
+++ src/main/java/com/sitenetsoft/opensource/videotron/services/VideotronServices.java
@@ -17,18 +17,74 @@ public class VideotronServices {
 
     public static final String module = VideotronServices.class.getName();
 
+    class CustomerCenterSections {
+        public String dashboard = "https://www.videotron.com/client/affaires/dashboard.do";
+        public String facture = "https://www.videotron.com/client/user-management/affaires/facture.do?dispatch=displayFacture";
+        public String paymentPreautorise = "https://www.videotron.com/client/affaires/secur/ppa/start.do";
+        public String rechercherNumerosPourUsage = "https://www.videotron.com/client/affaires/secur/wireless/rechercherNumerosPourUsage.do";
+        public String rechercherUsageDetails = "https://www.videotron.com/client/affaires/secur/wireless/rechercherUsageDetails.do";
+        public String profile = "https://www.videotron.com/client/affaires/profile.do?dispatch=initProfile&appId=EC";
+        public String displayAgreements = "https://www.videotron.com/client/affaires/agreements/secur/DisplayAgreements.action";
+        public String manageDomain = "https://www.videotron.com/client/user-management/affaires/secur/ManageDomain.do?dispatch=initDomainManagement";
+
+        public String = "https://www.videotron.com/client/affaires/linkdevicesim/listdevicesim.action"
+    }
+
+    class LoginFormInputs {
+        public String tokenName = "token_name";
+        public String tokenNameValue = "btoken";
+
+        public String btoken = "btoken"
+        public String btokenValue = "mwurcAAwpuJZIvjdkNAbiA9hL8ogy6sqEvnr";
+
+        public String dispatch = "dispatch";
+        public String dispatchValue = "login";
+
+        public String appId = "appId";
+        public String appIdValue = "EC";
+
+        public String domain = "domain";
+        public String domainValue = "domainType.business";
+
+        public String cookieEnabled = "cookieEnabled";
+        public String cookieEnabledValue = "false";
+
+        public String type = "type";
+        public String typeValue = "SSO-AFF-V1";
+
+        public String username = "username";
+        public String password = "password";
+    }
+
     public static Map<String, Object> pullDataVideotron(DispatchContext dctx, Map<String, ? extends Object> context) {
         Map<String, Object> result = ServiceUtil.returnSuccess();
         Delegator delegator = dctx.getDelegator();
+
         try {
 
             try {
+                /*Connection.Response loginForm = Jsoup.connect(CustomerCenterSections.class.dashboard)
+                        .method(Connection.Method.GET)
+                        .execute();*/
+
+                Document document = Jsoup.connect(CustomerCenterSections.class.dashboard)
+                        .data("cookieexists", "false")
+                        .data(LoginFormInputs.class.username, this.username)
+                        .data(LoginFormInputs.class.password, this.password)
+                        .cookies(loginForm.cookies())
+                        .post();
+                System.out.println(document);
+            } catch (Exception exception) {
+                System.out.println(exception);
+            }
+
+            /*try {
                 Document doc = Jsoup.connect("http://example.com").get();
                 doc.select("p").forEach(System.out::println);
                 System.out.println(doc.select("p").toString());
             } catch (Exception exception) {
                 System.out.println(exception);
-            }
+            }*/
 
             GenericValue videotron = delegator.makeValue("VideotronInvoices");
             // Auto generating next sequence of ofbizDemoId primary key
